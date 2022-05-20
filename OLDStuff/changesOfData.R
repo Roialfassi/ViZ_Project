affd93f2510a530e3483eeae3f62f5472de8dbb3
Roialfassi <roialfassi@gmail.com>
2022-05-20 17:29:27 +0300

    Website Form

--- OLDStuff/changesOfData.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-countries.info <-as_tibble(read.csv("firstData.csv", header=T))
-dictionary <- as_tibble(read.csv("dictionary.csv", header=T))
-
-countries.info<- countries.info %>% 
-  left_join(dictionary, by = c("Area" = "CountryName"))%>%
-  filter(CountryCode != "NA")
-write.csv(countries.info, "countriesWithCountryCode.csv")
